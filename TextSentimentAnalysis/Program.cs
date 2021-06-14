8af8bd2830abf35f4ad43989c64a651f04276646
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-06-14 10:40:11 +0200

    Add project files.

---
+++ TextSentimentAnalysis/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TextSentimentAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // 1. Utworzenie kontekstu (Create Context)
+
+            // 2. Ładowanie danych (Load data)
+
+            // 3. Przetwarzanie danych (Transform data)
+
+            // 4. Wybór algorytmu (Choose Algorithm)
+
+            // 5. Uczenie modelu (Train model)
+
+            // 6. Ocena modelu (Evaluate model)
+
+            // 7. Wdrożenie i konsumpcja modelu (Deploy & Consume)
+
+
+        }
+    }
+}
