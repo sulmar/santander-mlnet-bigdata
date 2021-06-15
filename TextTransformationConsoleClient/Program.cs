54913c40f799db3be07e2a532e81a5484529c2da
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-06-15 11:02:10 +0200

    Text Transformation

---
+++ TextTransformationConsoleClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.ML;
+using System;
+using System.Collections.Generic;
+
+namespace TextTransformationConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            IEnumerable<SentimentModel> messages = new SentimentModel[]
+            {
+                new SentimentModel { Text = "Hello World" },
+                new SentimentModel { Text = "Lorem ipsum" },
+                new SentimentModel { Text = "Hello ML.NET" },
+                new SentimentModel { Text = "Hello NET Core" },
+            };
+
+            // 1. Context
+            var context = new MLContext();
+
+            // 2. Load data
+            var dataView = context.Data.LoadFromEnumerable<SentimentModel>(messages);
+
+            var preview = dataView.Preview();
+
+
+
+        }
+    }
+
+    public class SentimentModel
+    {
+        public string Text { get; set; }
+    }
+}
