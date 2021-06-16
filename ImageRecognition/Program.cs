13de02d1de6537072dbd7b6273f65c9f3dc383aa
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-06-16 10:39:47 +0200

    Image Recognition

--- ImageRecognition/Program.cs
+++ ImageRecognition/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿using Microsoft.ML;
+using Microsoft.ML.Transforms.Image;
+using System;
+using System.Drawing;
+using Microsoft.ML.TensorFlow;
+using System.Collections.Generic;
+using Microsoft.ML.Data;
+using System.IO;
+using System.Linq;
 
 namespace ImageRecognition
 {
@@ -7,6 +15,98 @@ namespace ImageRecognition
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // TrainModel();
+
+            UseTrainedModel();
+
         }
+
+        private static void UseTrainedModel()
+        {
+            
+
+            string filename = @"C:\Users\marci\OneDrive\Obrazy\branding-car-on-the-street-mockup_77323-37.jpg";
+
+            // Context
+            var context = new MLContext();
+
+            // Load Model
+            var model = context.Model.Load("image-model.zip", out _);
+
+            // Engine
+            var engine = context.Model.CreatePredictionEngine<ImageInputData, ImageLabelPredictions>(model);
+
+            // Prediction
+            var bitmap = (Bitmap) Bitmap.FromFile(filename);
+
+            ImageInputData input = new ImageInputData { Image = bitmap };
+
+            var prediction = engine.Predict(input);
+
+            var labels = File.ReadAllLines(@"TFInceptionModel\imagenet_comp_graph_label_strings.txt");
+
+
+            var predictionLabels = prediction.PredictionLabels.OrderByDescending(p => p).Take(5);
+
+            foreach (var predictionLabel in predictionLabels)
+            {
+                var labelIndex = prediction.PredictionLabels.AsSpan().IndexOf(predictionLabel);
+
+                var classifiedLabel = labels[labelIndex];
+
+                Console.WriteLine($"Image from {filename} predicted as {classifiedLabel} with probability {predictionLabel:P2}");
+            }
+
+        }
+
+        private static void TrainModel()
+        {
+            const string tensorFlowModelFilePath = @"TFInceptionModel\tensorflow_inception_graph.pb";
+
+            // dotnet add package Microsoft.ML.ImageAnalytics
+
+            // 1. Context
+            var context = new MLContext();
+
+            // Transformacja
+            var pipeline = context.Transforms.ResizeImages(
+                "input",
+                imageWidth: 224,
+                imageHeight: 224,
+                nameof(ImageInputData.Image))
+                .Append(context.Transforms.ExtractPixels(
+                    "input",
+                    interleavePixelColors: true,
+                    offsetImage: 117
+                    ))
+                .Append(context.Model
+                    .LoadTensorFlowModel(tensorFlowModelFilePath)  // dotnet add package Microsoft.ML.TensorFlow
+                    .ScoreTensorFlowModel(
+                        outputColumnNames: new[] { "softmax2" },
+                        inputColumnNames: new[] { "input" },
+                        addBatchDimensionInput: true));
+
+            // Uwaga: dotnet add package SciSharp.TensorFlow.Redist
+
+            // Train model
+            IDataView emptyTrainingSet = context.Data.LoadFromEnumerable(new List<ImageInputData>());
+
+            ITransformer model = pipeline.Fit(emptyTrainingSet);
+
+            context.Model.Save(model, null, "image-model.zip");
+        }
+    }
+
+    public class ImageInputData
+    {
+        [ImageType(224, 244)]
+        public Bitmap Image { get; set; }
+    }
+
+    public class ImageLabelPredictions
+    {
+        [ColumnName("softmax2")]
+        public float[] PredictionLabels { get; set; }
     }
 }
